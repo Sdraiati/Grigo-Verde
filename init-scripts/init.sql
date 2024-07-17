fa80c3cfa8ba11920eb9d6f7801c6e6b06d6ee22
simocare <simone.caregnato@studenti.unipd.it>
2024-07-17 12:07:12 +0200

    init.sql

--- init-scripts/init.sql
+++ init-scripts/init.sql
@@ -2,25 +2,41 @@ DROP DATABASE IF EXISTS scaregna;
 CREATE DATABASE scaregna;
 USE scaregna;
 
--- creazione tabella utente
-CREATE TABLE user (
-                        id INT NOT NULL AUTO_INCREMENT,
-                        username VARCHAR(255) NOT NULL UNIQUE,
-                        password VARCHAR(255) NOT NULL,
-                        PRIMARY KEY (id)
+CREATE TABLE UTENTE (
+    Username VARCHAR(50) PRIMARY KEY,
+    Password varchar(256) NOT NULL,
+    Nome VARCHAR(100) NOT NULL,
+    Cognome VARCHAR(100) NOT NULL,
+    Ruolo VARCHAR(50) NOT NULL
 );
 
--- Inserimento dati di esempio per la tabella utente
-INSERT INTO utente (email, username, password)
-VALUES
-    ('marco', 'password'),
-    ('anna', 'password'),
-    ('luca', 'password'),
-    ('giulia', 'password'),
-    ('federico', 'password'),
-    ('marta', 'password'),
-    ('roberto', 'password'),
-    ('chiara', 'password'),
-    ('francesco', 'password'),
-    ('laura', 'password'),
-    ('user', 'user');
+CREATE TABLE SPAZIO (
+    Posizione INT PRIMARY KEY,
+    Nome VARCHAR(100) NOT NULL,
+    Descrizione TEXT DEFAULT "",
+    Tipo VARCHAR(50) NOT NULL,
+    N_tavoli INT DEFAULT 0
+);
+
+CREATE TABLE PRENOTAZIONE (
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    Data DATETIME NOT NULL,
+    Username VARCHAR(50) NOT NULL REFERENCES UTENTE(Username),
+    Posizione_spazio INT NOT NULL REFERENCES UTENTE(Posizione)
+);
+
+CREATE TABLE IMMAGINE (
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    Dato LONGBLOB NOT NULL,
+    Alt VARCHAR(100) DEFAULT "",
+    Mime_type VARCHAR(30) NOT NULL,
+    Posizione_spazio INT NOT NULL
+);
+
+CREATE TABLE DISPONIBILITA (
+    Posizione_spazio INT NOT NULL,
+    Mese ENUM('Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'),
+    Giorno_settimana ENUM('Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì', 'Sabato', 'Domenica'),
+    Orario_apertura TIME,
+    Orario_chiusura TIME
+)
