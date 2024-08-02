9700da9084cb9cbed842a4cf206c4b9c2f8faf0e
simocare <simone.caregnato@studenti.unipd.it>
2024-08-02 15:38:22 +0200

    controllo degli input front-end, i messaggi sono generici per il login, ma potranno essere specifici per la registrazione dell'utente

--- assets/script.js
+++ assets/script.js
@@ -21,3 +21,79 @@ function toggleView(name = "password") {
         }
     }
 }
+
+function insertErrorMessage(element, msg) {
+    let error_div = document.createElement('div');
+    error_div.className = 'errore';
+
+    let error_message = document.createElement('p');
+    error_message.innerHTML = msg;
+
+    error_div.appendChild(error_message);
+
+    element.parentNode.insertBefore(error_div, element.nextSibling);
+}
+
+function removeErrorDivs() {
+    let error_divs = document.getElementsByClassName("errore");
+    for (let i = 0; i < error_divs.length; i++) {
+        error_divs[i].remove();
+    }
+}
+
+function validateString(element, str, min=Number.MIN_SAFE_INTEGER, max=Number.MAX_SAFE_INTEGER, show_char_number=false) {
+    const specialCharPattern = /[{}\[\]|`¬¦!"£$%^&*<>:;#~_\-+=,@]/;
+    if(str === '') {
+        insertErrorMessage(element, "Il campo non può essere vuoto.");
+        return false;
+    }
+
+    if(str.length < min) {
+        if (show_char_number) {
+            insertErrorMessage(element,"Il campo deve contenere almeno " + min + " caratteri.");
+        }
+        return false;
+    }
+    if(str.length > max) {
+        if (show_char_number) {
+            insertErrorMessage(element,"Il campo deve contenere al massimo " + max + " caratteri.");
+        }
+        return false;
+    }
+    // se la stringa contiene uno spazio
+    if (str.includes(' ')) {
+        if(show_char_number) {
+            insertErrorMessage(element,"Il campo non può contenere spazi.");
+        }
+        return false;
+    }
+
+    if (specialCharPattern.test(str)) {
+        if(show_char_number) {
+            insertErrorMessage(element,"Il campo non può contenere caratteri speciali.");
+        }
+        return false;
+    }
+
+    return true;
+}
+
+function validateLogin() {
+    let username = document.getElementsByName("username")[0].value;
+    let password = document.getElementsByName("password")[0].value;
+
+    let username_element = document.getElementsByName("username")[0];
+    let password_element = document.getElementsByName("password")[0];
+
+    let fieldset_element = document.getElementsByTagName("fieldset")[0];
+
+    let isUsernameValid = validateString(fieldset_element, username, 4, 50);
+    let isPasswordValid = validateString( fieldset_element, password,  4, 100);
+
+    removeErrorDivs();
+    if (!isUsernameValid || !isPasswordValid) {
+        insertErrorMessage(fieldset_element,"Username o password non validi");
+    }
+
+    return isUsernameValid && isPasswordValid;
+}
