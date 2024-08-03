fe5a35f4c60624d7458bdab828e7ce128404fb98
bassupreme <leonardo.basso.7@studenti.unipd.it>
2024-08-03 10:35:07 +0200

    Aggiunti i campi privati alla classe VisualizzazioneSpaziPage; questo serve alla get request.

--- src/page/visualizzazioneSpaziPage.php
+++ src/page/visualizzazioneSpaziPage.php
@@ -15,6 +15,8 @@ class SpazioItem {
 
 class VisualizzazioneSpaziPage extends Page
 {
+    private string $tipo;
+    private $data;
     public $title = 'VisualizzazioneSpazi';
     public $nav = [
         'About us' => 'about_us',
@@ -25,13 +27,15 @@ class VisualizzazioneSpaziPage extends Page
     public $keywords = ["Grigo verde", "aule verdi", "Liceo Scientifico", "M. Grigoletti", "scuola superiore", "Pordenone", "prenotazione", "area ping pong"];
     public $path = '/visualizzazione_spazi';
 
-    public function __construct() {
+    public function __construct(string $tipo = "", string $data = "") {
         parent::setTitle('Viualizzazione Spazi');
         parent::setNav([]);
         parent::setBreadcrumb([
             'Home' => '',
         ]);
 
+        $this->$tipo = $tipo;
+        $this->$data = $data;
     }
 
     public function replace_content($content) {
@@ -55,9 +59,9 @@ class VisualizzazioneSpaziPage extends Page
         $model_spazio = new Spazio();
         $items =  $model_spazio->prendi_tutti(); // questo è un array di SpazioItems che deve essere rimpiazzato a {{ content }}
         
-        
-        // $content = str_replace("{{ content }}", $this->getContent('home'), $content);
-        // $content = str_replace("href=\"/\"", "href=\"#\"", $content);
+        // contenuto che varia in base agli spazi.
+        $content = str_replace("{{ content }}", $this->getContent('home'), $content);
+        $content = str_replace("href=\"/\"", "href=\"#\"", $content);
 
         $content = str_replace('{{ base_path }}', BASE_URL, $content);
         $content = str_replace("{{ error }}", '', $content);
