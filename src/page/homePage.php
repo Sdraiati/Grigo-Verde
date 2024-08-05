dfb940ad998f5e858272740e8783b38ef8eddc37
simocare <simone.caregnato@studenti.unipd.it>
2024-08-05 23:06:21 +0200

    utilizzo dei metodi che erano stati creati in page e sistemazione di span, navbar e bread

--- src/page/homePage.php
+++ src/page/homePage.php
@@ -6,29 +6,24 @@ class HomePage extends Page
 {
     public $title = 'Home';
     public $nav = [
-        'About us' => 'about_us',
-        'Login' => 'login'
+        '<span lang="en">About us</span>' => 'about_us',
+        '<span lang="en">Login</span>' => 'login'
     ];
     public $breadcrumb = [];
     public $keywords = ["Grigo verde", "aule verdi", "Liceo Scientifico", "M. Grigoletti", "scuola superiore", "Pordenone", "prenotazione", "area ping pong"];
     public $path = '/';
 
-    public function __construct() {}
+    public function __construct() {
+        $this->setTitle($this->title);
+        $this->setNav($this->nav);
+        $this->setBreadcrumb($this->breadcrumb, true);
+        $this->addKeywords($this->keywords);
+        $this->setPath($this->path);
+    }
 
     public function render()
     {
-        $content = $this->getContent('layout');
-        $content = str_replace('{{ title }}', $this->title . ' - Grigo Verde', $content);
-        $content = str_replace('{{ description }}', 'This is a description', $content);
-        $content = str_replace('{{ keywords }}', implode(', ', $this->keywords), $content);
-        $content = str_replace('{{ page_path }}', $this->path, $content);
-
-        $nav = new ReferenceList($this->nav);
-        $content = str_replace('{{ menu }}', $nav->render(), $content);
-
-        $breadcrumb = new Breadcrumb($this->breadcrumb, $this->title);
-        $content = str_replace('{{ breadcrumbs }}', $breadcrumb->render(), $content);
-        $content = $this->takeOffCircularReference($content);
+        $content = parent::render();
 
         $content = str_replace("{{ content }}", $this->getContent('home'), $content);
         $content = str_replace("href=\"/\"", "href=\"#\"", $content);
