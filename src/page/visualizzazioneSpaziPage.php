4182ba4fdbd636b8c2df095396ad248911db4819
bassupreme <leonardo.basso.7@studenti.unipd.it>
2024-08-09 11:13:40 +0200

    rimosso codice morto da visualizzazioneSpaziPage.php.

--- src/page/visualizzazioneSpaziPage.php
+++ src/page/visualizzazioneSpaziPage.php
@@ -176,18 +176,11 @@ class VisualizzazioneSpaziPage extends Page
             $start_picker_r = '<input type="time" id="orario_inizio" name="orario_inizio" value="' . $start .'">';
             $end_picker = '<input type="time" id="orario_fine" name="orario_fine">';
             $end_picker_r = '<input type="time" id="orario_fine" name="orario_fine" value="' . $end .'">';
-            // $unchecked = '<input type="datetime-local" name="Data_inizio" id="Data_inizio" />';
-            // $checked = '<input type="datetime-local" name="Data_inizio" id="Data_inizio" value="' . $this->data_inizio . '"/>';
+
             $intestazione_pagina = str_replace($date_picker, $date_picker_r, $intestazione_pagina);
             $intestazione_pagina = str_replace($start_picker, $start_picker_r, $intestazione_pagina);
             $intestazione_pagina = str_replace($end_picker, $end_picker_r, $intestazione_pagina);
         }
-
-        // if ($this->data_fine != "") {
-        //     $unchecked = '<input type="datetime-local" name="Data_fine" id="Data_fine" />';
-        //     $checked = '<input type="datetime-local" name="Data_fine" id="Data_fine" value="' . $this->data_fine . '"/>';
-        //     $intestazione_pagina = str_replace($unchecked, $checked, $intestazione_pagina);
-        // }
 
         // lista degli spazi
         $lista_debug = $this->filtra_spazi($this->tipo, $this->data_inizio, $this->data_fine);
