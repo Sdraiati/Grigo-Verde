09e3eafb9bca62c4b079b8dec7a9396b679d0aed
bassupreme <leonardo.basso.7@studenti.unipd.it>
2024-08-09 15:21:13 +0200

    Refactoring della funzione VisualizzazioneSpaziPage::render() e della pagina html.

--- src/page/visualizzazioneSpaziPage.php
+++ src/page/visualizzazioneSpaziPage.php
@@ -7,18 +7,17 @@ include_once 'model/spazio.php';
 // classe item
 class SpazioItem {
 
-    // renderizza uno spazio
-    // $params: array contenente i valori dei campi spazio
     public function render($values) {
         $item = '<li id="' . $values["Posizione"] . '">' . $values["Nome"];
         if ($values["Byte"]) {
             $item = $item . '<img src="' . $values["Byte"] .' alt="">';
-        } else { // immagine di default
+        } else { 
             $item = $item . '<img src="assets/default_space_light_theme.png" alt="">';
         }
         $item = $item . ' </li>';
         return $item;
     }
+
 }
 
 class VisualizzazioneSpaziPage extends Page
@@ -75,20 +74,13 @@ class VisualizzazioneSpaziPage extends Page
             $stmt->execute();
             $result = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 
-            // DEBUG
-            // var_dump($tipo);
-            // var_dump($data_inizio);
-            // var_dump($data_fine);
-
             if ($tipo == "" && $data_inizio == "" && $data_fine == "") {
                 return $result;
             }
             $filtered = [];
 
-            //var_dump($result);
-
             if ($tipo != "" || $data_inizio != "" || $data_fine != "") {
-                $filtered = $result; // questo è da cambiare.
+                $filtered = $result; 
 
                 if ($tipo != "") {
                     $filtered = [];
@@ -161,66 +153,46 @@ class VisualizzazioneSpaziPage extends Page
     public function render()
     {
         $content = parent::render();
-
         $intestazione_pagina = $this->getContent('visualizzazione_spazi_page');
 
-        // Renderizzare i filtri se ce ne sono. 
+        // Renderizzare i filtri attivi se ce ne sono. 
         if ($this->tipo != "") {
-            $checked = '<input type="radio" id="tipo_' . $this->tipo .  '" name="tipo" value="' . $this->tipo . '" checked>';
-            $unchecked = '<input type="radio" id="tipo_' . $this->tipo . '" name="tipo" value="' . $this->tipo . '">';
-            $intestazione_pagina = str_replace($unchecked, $checked, $intestazione_pagina);
+            $intestazione_pagina = str_replace('{{ checked-' . $this->tipo . ' }}', 'checked', $intestazione_pagina);
         }
-
         if ($this->data_inizio != "" && $this->data_fine != "") { 
             $data = explode(" ", $this->data_inizio)[0];
             $start = explode(" ", $this->data_inizio)[1];
             $end = explode(" ", $this->data_fine)[1];
 
-            $date_picker = '<input type="date" id="data" name="data"/>';
-            $date_picker_r = '<input type="date" id="data" name="data" value="' . $data .  '"/>';
-            $start_picker = '<input type="time" id="orario_inizio" name="orario_inizio">';
-            $start_picker_r = '<input type="time" id="orario_inizio" name="orario_inizio" value="' . $start .'">';
-            $end_picker = '<input type="time" id="orario_fine" name="orario_fine">';
-            $end_picker_r = '<input type="time" id="orario_fine" name="orario_fine" value="' . $end .'">';
-
-            $intestazione_pagina = str_replace($date_picker, $date_picker_r, $intestazione_pagina);
-            $intestazione_pagina = str_replace($start_picker, $start_picker_r, $intestazione_pagina);
-            $intestazione_pagina = str_replace($end_picker, $end_picker_r, $intestazione_pagina);
+            $intestazione_pagina = str_replace('{{ data }}', 'value="' . $data . '"', $intestazione_pagina);
+            $intestazione_pagina = str_replace('{{ start }}', 'value="' . $start . '"', $intestazione_pagina);
+            $intestazione_pagina = str_replace('{{ end }}', 'value="' . $end . '"', $intestazione_pagina);
         }
 
         // lista degli spazi
-        $lista_debug = $this->filtra_spazi($this->tipo, $this->data_inizio . ":00", $this->data_fine . ":00");
+        $query_result = $this->filtra_spazi($this->tipo, $this->data_inizio . ":00", $this->data_fine . ":00");
 
-        if ($lista_debug) {
+        if ($query_result) {
             $lista_spazi = "";
             $spazioItem = new SpazioItem();
-            for ($i = 0; $i < count($lista_debug); $i++) {
+            for ($i = 0; $i < count($query_result); $i++) {
                 $values = [];
-                $values["Posizione"] = ($lista_debug[$i]["Posizione"]);
-                $values["Nome"] = ($lista_debug[$i]["Nome"]);
-                $values["Byte"] = ($lista_debug[$i]["Byte"]);
+                $values["Posizione"] = ($query_result[$i]["Posizione"]);
+                $values["Nome"] = ($query_result[$i]["Nome"]);
+                $values["Byte"] = ($query_result[$i]["Byte"]);
                 $lista_spazi = $lista_spazi . $spazioItem->render($values);
             }
 
-            if ($lista_spazi == "") {
-                $lista_spazi = "non sono stati trovai degli spazi corrispondenti ai parametri della ricerca";
-            }
-
             $intestazione_pagina = str_replace("{{ lista }}", $lista_spazi, $intestazione_pagina);
-
             $content = str_replace("{{ content }}", $intestazione_pagina, $content);
-            $content = str_replace("href=\"/\"", "href=\"#\"", $content);   // todo: check if and why this is needed (this should never be needed)
-            $content = str_replace('{{ base_path }}', BASE_URL, $content);  // todo: check if and why this is needed
         } else {
             $messaggio = " <p> non sono stati trovai degli spazi corrispondenti ai parametri della ricerca <p>";
-
             $intestazione_pagina = str_replace("{{ lista }}", $messaggio, $intestazione_pagina);
-
             $content = str_replace("{{ content }}", $intestazione_pagina, $content);
-            $content = str_replace("href=\"/\"", "href=\"#\"", $content);   // idem as above
-            $content = str_replace('{{ base_path }}', BASE_URL, $content);  // idem as above
-
         }
+
+        $content = str_replace("href=\"/\"", "href=\"#\"", $content);   // todo: check if and why this is needed (this should never be needed)
+        $content = str_replace('{{ base_path }}', BASE_URL, $content);  // todo: check if and why this is needed
         if ($this->error) {
             $content = str_replace("{{ error }}", $this->error, $content);            // idem as above
         } else {
