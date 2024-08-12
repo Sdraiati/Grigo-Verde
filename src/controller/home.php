ca6442e1e9666f21bd1a049e4816165c852e020d
Carlo Rosso <carlorosso@Carlos-MacBook-Air.local>
2024-08-12 15:52:01 +0200

    il basepath del sito viene controllato solo in global_values.php

---
+++ src/controller/home.php
@@ -0,0 +1,40 @@
+<?php
+
+$project_root = dirname(__FILE__, 2);
+include_once 'endpoint.php';
+include_once $project_root . '/page/homePage.php';
+
+class Home extends Endpoint
+{
+    public function __construct()
+    {
+        parent::__construct('', 'GET');
+    }
+
+    public function match($path, $method): bool
+    {
+        $path = rtrim($path, '/');
+
+        $uri = parse_url(BASE_URL . $this->path);
+        $endpoint = '';
+
+        if (isset($uri['path'])) {
+            $endpoint = $uri['path'];
+        }
+        if (isset($uri['query'])) {
+            $endpoint .= '?' . $uri['query'];
+        }
+
+        return parent::match($path, $method);
+    }
+
+    public function handle()
+    {
+        $page = new HomePage();
+        $page->setPath('');
+        $html = $page->render();
+        $html = str_replace('href="#"', 'href="' . rtrim(BASE_URL, '/') . '#"', $html);
+        $html = str_replace('href="#content"', 'href="' . rtrim(BASE_URL, '/') . '#content"', $html);
+        echo $html;
+    }
+}
